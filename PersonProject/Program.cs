f7a55bc0f6e8c7795c21a6605cf651ad36967468
meerzaheen_90 <meerzaheen_90@hotmail.com>
2016-06-10 12:25:29 -0400

    started abstract

--- PersonProject/Program.cs
+++ PersonProject/Program.cs
@@ -10,6 +10,9 @@ namespace PersonProject
     {
         static void Main(string[] args)
         {
+            Person newPerson = new Person(25, "Zaheen");
+            Student Meer = new Student(26, "Meer", "A123456");
+            Console.WriteLine(Meer.ToString());
         }
     }
 }
